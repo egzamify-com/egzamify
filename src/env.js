0887a57f90a10ecdfbd046b3b54f11bf940469fd
antoni-ostrowski <antost360@gmail.com>
2025-05-29 17:03:07 +0200

    got rid of not needed env

--- src/env.js
+++ src/env.js
@@ -8,7 +8,6 @@ export const env = createEnv({
    */
   server: {
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string().url(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -31,7 +30,6 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
