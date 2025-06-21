46b6b92f58f1657167abd37862bd52293b97ce32
antoni-ostrowski <antost360@gmail.com>
2025-06-21 20:49:38 +0200

    delete old comments, linting

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -29,7 +29,6 @@ export const usersRouter = createTRPCRouter({
       async ({
         input,
         ctx: {
-          db,
           auth: {
             user: { id: currentUserId },
           },
@@ -40,24 +39,10 @@ export const usersRouter = createTRPCRouter({
         const searchTerm =
           input.search.trim().length > 0 ? `%${input.search}%` : "%%";
 
-        // const baseFilters = {
-        //   orderBy: [user.email], // Or any other sorting for consistency
-        //   limit: limit + 1, // Fetch one more than limit to check if there's a next page
-        //   offset: offset,
-        // };
         console.log("seaching for ->", `${searchTerm}`);
         console.log("quering with -> ", `currentUserID${currentUserId}`);
         console.log("quering for -> ", input.filter);
-        // const [currentUsersFriendsIds, currentUsersFriendsError] =
-        //   await tryCatch(getCurrentUsersFriendsIds(currentUserId));
 
-        // if (currentUsersFriendsError) {
-        //   throw new TRPCError({
-        //     code: "INTERNAL_SERVER_ERROR",
-        //     cause: currentUsersFriendsError.cause,
-        //     message: currentUsersFriendsError.message,
-        //   });
-        // }
         let queryUsers;
         let queryError;
 
@@ -109,39 +94,6 @@ export const usersRouter = createTRPCRouter({
         }
         console.log("query error:", queryError);
         console.log("query users:", queryUsers);
-        // if (!searchTerm) {
-        //   // If no search term, return all users (with pagination)
-        //   [queryUsers, queryError] = await tryCatch(
-        //     db.query.user.findMany({
-        //       orderBy: [user.email], // Or any other sorting for consistency
-        //       limit: limit + 1, // Fetch one more than limit to check if there's a next page
-        //       offset: offset,
-        //     }),
-        //   );
-        // } else {
-        //   // Otherwise, perform the search with `ilike` and `or`
-        //   [queryUsers, queryError] = await tryCatch(
-        //     db.query.user.findMany({
-        //       where: (usersTable, { ilike, or }) =>
-        //         or(
-        //           ilike(usersTable.username, `%${searchTerm}%`),
-        //           ilike(usersTable.email, `%${searchTerm}%`),
-        //           // Add other fields as needed
-        //         ),
-        //       orderBy: [user.email], // Or any other sorting for consistency
-        //       limit: limit + 1, // Fetch one more than limit to check if there's a next page
-        //       offset: offset,
-        //     }),
-        //   );
-        // }
-
-        // if (queryError) {
-        //   throw new TRPCError({
-        //     code: "INTERNAL_SERVER_ERROR",
-        //     cause: queryError.cause,
-        //     message: queryError.message,
-        //   });
-        // }
 
         // Determine if there's a next page
         let nextCursor: typeof input.cursor | undefined = undefined;
@@ -151,16 +103,6 @@ export const usersRouter = createTRPCRouter({
           nextCursor = offset + limit; // The offset for the next page
         }
 
-        // const usersWithFriendStatus =
-        //   queryUsers?.map((user) => ({
-        //     ...user,
-        //     isFriendWithCurrentUser: currentUsersFriendsIds.includes(user.id),
-        //   })) ?? [];
-
-        // const onlyUsersFriends = usersWithFriendStatus.filter(
-        //   (user) => user.isFriendWithCurrentUser,
-        // );
-
         return {
           items: queryUsers,
           nextCursor: nextCursor, // This is the single nextCursor for the entire page
@@ -168,60 +110,7 @@ export const usersRouter = createTRPCRouter({
       },
     ),
 });
-// async function getCurrentUsersFriendsIds(currentUserId: string) {
-//   const [currentUsersFriendsFromRequest, currentUsersFriendsFromRequestError] =
-//     await tryCatch(
-//       db.query.friend.findMany({
-//         columns: {},
-//         where: () => eq(friend.requesting_user_id, currentUserId),
-//         with: {
-//           receivingUser: {
-//             columns: {
-//               id: true,
-//             },
-//           },
-//         },
-//       }),
-//     );
-//   const [currentUsersFriendsFromReceive, currentUsersFriendsFromReceiveError] =
-//     await tryCatch(
-//       db.query.friend.findMany({
-//         columns: {},
-//         where: () => eq(friend.receiving_user_id, currentUserId),
-//         with: {
-//           requestingUser: {
-//             columns: {
-//               id: true,
-//             },
-//           },
-//         },
-//       }),
-//     );
 
-//   if (currentUsersFriendsFromRequestError) {
-//     throw new TRPCError({
-//       code: "INTERNAL_SERVER_ERROR",
-//       cause: currentUsersFriendsFromRequestError.cause,
-//       message: currentUsersFriendsFromRequestError.message,
-//     });
-//   }
-//   if (currentUsersFriendsFromReceiveError) {
-//     throw new TRPCError({
-//       code: "INTERNAL_SERVER_ERROR",
-//       cause: currentUsersFriendsFromReceiveError.cause,
-//       message: currentUsersFriendsFromReceiveError.message,
-//     });
-//   }
-
-//   const fromReqIds = currentUsersFriendsFromRequest?.map(
-//     (user) => user.receivingUser.id,
-//   );
-//   const fromRecIds = currentUsersFriendsFromReceive?.map(
-//     (user) => user.requestingUser.id,
-//   );
-
-//   return [...fromReqIds, ...fromRecIds];
-// }
 async function getCurrentUsersFriends(
   currentUserId: string,
   limit: number,
