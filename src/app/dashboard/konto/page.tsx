5cc677c16dae462ec4e93cf86c1cb5cbfe1cb4c7
antoni-ostrowski <antost360@gmail.com>
2025-07-23 11:17:20 +0200

    update account page to new auth

--- src/app/dashboard/konto/page.tsx
+++ src/app/dashboard/konto/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import { api } from "convex/_generated/api";
+import { useQueryWithStatus } from "convex/helpers";
 import { redirect } from "next/navigation";
+import SpinnerLoading from "~/components/SpinnerLoading";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Card, CardContent } from "~/components/ui/card";
-import { authClient } from "~/lib/auth-client";
 import UpdateProfileDialog from "./UpdateProfile";
 
 export default function Page() {
-  const auth = authClient.useSession();
-  const user = auth.data?.user;
-  if (!user && !auth.isPending) return redirect("/sign-in");
+  const {
+    data: user,
+    error,
+    isPending,
+  } = useQueryWithStatus(api.users.query.getCurrentUser);
+
+  if (isPending) {
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <SpinnerLoading />
+      </div>
+    );
+  }
+  if (!user || error) return redirect("/sign-in");
 
   return (
-    <div className="pt-10 px-10 space-y-6">
+    <div className="space-y-6 px-10 pt-10">
       <Card className="w-full">
         <CardContent className="p-6">
           <div className="flex items-center space-x-4">
@@ -21,13 +34,13 @@ export default function Page() {
                 src="/placeholder.svg?height=80&width=80"
                 alt="Profile picture"
               />
-              <AvatarFallback className="text-lg font-semibold bg-blue-100 text-blue-600">
+              <AvatarFallback className="text-lg font-semibold">
                 AS
               </AvatarFallback>
             </Avatar>
             <div className="flex-1">
-              <h1 className="text-2xl font-bold text-gray-900">{user?.name}</h1>
-              <p className="text-gray-600 mt-1">{user?.email}</p>
+              <h1 className="text-2xl font-bold">{user?.username}</h1>
+              <p className="text-muted-foreground mt-1">{user?.email}</p>
             </div>
             <UpdateProfileDialog />
           </div>
