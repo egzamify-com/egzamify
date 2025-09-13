1960ee779bb7f3ec873a2eb4cffc9c016e4feb6a
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2025-09-13 13:21:56 +0200

    ts fix

--- src/app/LandingPage.tsx
+++ src/app/LandingPage.tsx
@@ -1,16 +1,5 @@
 "use client";
 
-import useAuth from "~/hooks/useAuth";
-
 export default function LandingPage() {
-  
-  
-
-  
-  
-
-  return <main className="bg-green-100">Landing page
-  
-  
-  </main>;
+  return <main className="bg-green-100">Landing page</main>;
 }
