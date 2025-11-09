4dfba425f78bd713239359af1ef890784232645d
antoni-ostrowski <antost360@gmail.com>
2025-11-09 15:04:33 +0100

    payments: unblock payment buttons

--- src/app/pricing/product.tsx
+++ src/app/pricing/product.tsx
@@ -107,7 +107,7 @@ export default function Product({
           className={`h-14 w-full text-lg font-semibold`}
           size="lg"
           onClick={async () => await handleCheckout()}
-          disabled={true}
+          disabled={mutationPending}
         >
           {mutationPending ? <SpinnerLoading /> : <>Zakup</>}
         </Button>
