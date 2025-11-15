9e62612e87ea79af641b4843d37c85a811fb375d
antoni-ostrowski <antost360@gmail.com>
2025-11-15 15:48:59 +0100

    pvp-quiz: better handling for error and pending states for qualification input

--- src/app/dashboard/online/pvp-quiz/(components)/select-qualification-for-pvp-quiz.tsx
+++ src/app/dashboard/online/pvp-quiz/(components)/select-qualification-for-pvp-quiz.tsx
@@ -1,9 +1,9 @@
 import { api } from "convex/_generated/api"
 import { useQuery } from "convex/custom_helpers"
 import { transformQualificationsToOptions } from "~/components/settings/update-qualifications"
-import SpinnerLoading from "~/components/spinner-loading"
 import { Field, FieldLabel, FieldSet } from "~/components/ui/field"
 import { MultiSelect } from "~/components/ui/multi-select"
+import { Skeleton } from "~/components/ui/skeleton"
 
 export default function SelectQualificationForPvpQuiz({
   selectedQualification,
@@ -13,28 +13,34 @@ export default function SelectQualificationForPvpQuiz({
   handleNewQualification: (newValue: string[]) => void
 }) {
   const qualificationsQuery = useQuery(api.users.query.getSavedQualifications)
-  if (qualificationsQuery.isPending) {
-    return <SpinnerLoading />
-  }
-  if (qualificationsQuery.error || !qualificationsQuery.data) {
-    return <div>error with qualifications</div>
-  }
+  const isQueryError =
+    (qualificationsQuery.error || !qualificationsQuery.data) &&
+    !qualificationsQuery.isPending
+
   return (
     <FieldSet>
       <Field>
         <FieldLabel htmlFor="multi-select-pvp-quiz">Kwalifikacja</FieldLabel>
-        <MultiSelect
-          className="bg-input/30 h-9 p-0"
-          id="multi-select-pvp-quiz"
-          singleSelect
-          options={transformQualificationsToOptions(
-            qualificationsQuery.data.allQualifications,
-          )}
-          onValueChange={handleNewQualification}
-          value={selectedQualification}
-          hideSelectAll
-          placeholder="Wybierz kwalifikacje"
-        />
+        {qualificationsQuery.isPending && <Skeleton className="h-9 w-full" />}
+        {isQueryError && (
+          <div className="text-destructive">
+            <p>{"Wystapił nieoczekiwany bład."}</p>
+          </div>
+        )}
+        {!isQueryError && qualificationsQuery.data && (
+          <MultiSelect
+            className="bg-input/30 h-9 p-0"
+            id="multi-select-pvp-quiz"
+            singleSelect
+            options={transformQualificationsToOptions(
+              qualificationsQuery.data.allQualifications,
+            )}
+            onValueChange={handleNewQualification}
+            value={selectedQualification}
+            hideSelectAll
+            placeholder="Wybierz kwalifikacje"
+          />
+        )}
       </Field>
     </FieldSet>
   )
