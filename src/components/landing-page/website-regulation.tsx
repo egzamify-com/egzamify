fa6746106d7994e7ed7742855f6fef47c750ad37
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2025-11-27 23:31:57 +0100

    comment added

--- src/components/landing-page/website-regulation.tsx
+++ src/components/landing-page/website-regulation.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 export default function WebsiteRegulation() {
   return (
+    //sasa
     <Link href="/regulamin-serwisu" className="underline">
       Regulamin serwisu
     </Link>
