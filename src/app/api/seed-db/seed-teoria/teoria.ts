10976dda8dc81745b8c3c8a7b6107a51c9cddd0b
antoni-ostrowski <antost360@gmail.com>
2025-12-07 18:33:21 +0100

    rm stuff

--- src/app/api/seed-db/seed-teoria/teoria.ts
+++ src/app/api/seed-db/seed-teoria/teoria.ts
@@ -28,7 +28,6 @@ async function main() {
       body: data,
       headers: {
         Authorization: `Bearer ${secret}`,
-        // Cookie: `__convexAuthJWT=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJtNTczanFlZjZobTAwZGpxNTR6N2NiYTgyOTd3NXdwc3xqbjc3OWN5eGpybTgxZXd4NWN3MzRobWh3OTd3bnMwOCIsImlhdCI6MTc2NTEyMTY4MiwiaXNzIjoiaHR0cHM6Ly9mb3JtYWwtamF5LTE0Ni5jb252ZXguc2l0ZSIsImF1ZCI6ImNvbnZleCIsImV4cCI6MTc2NTEyNTI4Mn0.mYL-m6320fy5WZDXwauSTPpuCnZ4fTc0z5Bb2_f2KQfse7qsXrfEkY1EvEl2ylp7rVCXgMmNoJs5LrtzAYkd4crvPDxsVq3abWhh2zLs1uOZjUdPbAB_sLtoUfnx1OvpQ1X-jDXdS6Xl2KTce2-PQ27aHYaW3Em1JAHSSSwCMR_7Z1o0-r4igEEBhc6gs8Zc6h2qehJuELQ9VouJIJdPf5sAfSk0aq7fB6Sh7NEOcy2uiAibuZ4oAbYrN70JftE8xEEFxd1bCeANJ76fvncoRkiQ1db7uCr_SXRXVTpfblNb_tjQ7VFp7UymizFbhws1NmwpL6AkruV21uByAsBMMg`,
       },
     },
   )
