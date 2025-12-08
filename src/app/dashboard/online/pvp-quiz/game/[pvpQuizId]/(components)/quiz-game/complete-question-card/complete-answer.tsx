2886e5e4f90e19a7590ceba1961ecbb83b370269
antoni-ostrowski <antost360@gmail.com>
2025-12-08 22:52:59 +0100

    complete-question-comp: add images in answers

--- src/app/dashboard/online/pvp-quiz/game/[pvpQuizId]/(components)/quiz-game/complete-question-card/complete-answer.tsx
+++ src/app/dashboard/online/pvp-quiz/game/[pvpQuizId]/(components)/quiz-game/complete-question-card/complete-answer.tsx
@@ -1,10 +1,11 @@
 import type { api } from "convex/_generated/api"
 import type { QuizAnswersType } from "convex/online/pvp_quiz/helpers"
 import type { FunctionReturnType } from "convex/server"
+import Image from "next/image"
 import MarkdownRenderer from "~/components/markdown-rendered"
 import { Badge } from "~/components/ui/badge"
 import ActivityStatusAvatar from "~/components/users/activity-status-avatar"
-import { cn } from "~/lib/utils"
+import { cn, getFileUrl } from "~/lib/utils"
 import type { CompleteQuestionProps } from "./complete-question"
 
 export default function Answer({
@@ -53,6 +54,8 @@ export default function Answer({
       didCurrentUserSelectThisAnswer) ||
     isThereAnyOtherUserAnswer
 
+  const answerAttachment = getFileUrl(answer.attachmentId, "image")?.raw.href
+
   return (
     <div
       onClick={() => {
@@ -79,14 +82,31 @@ export default function Answer({
       )}
       <div className={cn(`w-full flex-row items-center justify-center`)}>
         <div className={`flex w-full flex-row items-center justify-between`}>
-          <div className="flex flex-row items-center justify-center gap-2">
-            <p className="text-xl">{answer.label}</p>
-            <span className="text-foreground text-md leading-relaxed text-pretty">
-              <MarkdownRenderer
-                markdownText={answer.content}
-                textSize="prose-md"
-              />
-            </span>
+          <div className="flex flex-col items-start justify-center gap-2">
+            <div className="flex flex-row items-center justify-center gap-2">
+              <p className="text-xl">{answer.label}</p>
+              <span className="text-foreground text-md leading-relaxed text-pretty">
+                <MarkdownRenderer
+                  markdownText={answer.content}
+                  textSize="prose-md"
+                />
+              </span>
+            </div>
+
+            {answerAttachment && (
+              <div className="mx-auto max-w-md overflow-hidden rounded-xl md:max-w-2xl">
+                <div className="md:flex">
+                  <div className="relative mx-auto flex max-h-[36rem] min-h-[16rem] w-full max-w-xl min-w-[16rem] items-center justify-center overflow-hidden rounded-lg">
+                    <Image
+                      alt="Obraz do pytania"
+                      fill
+                      src={answerAttachment}
+                      className="object-contain"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
           {questionComponentProps.nonInteractive && (
             <div
